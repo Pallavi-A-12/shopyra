4f7a01304bf1e67a3b8b1ab580a81382024700a4
Pallavi-A-12 <pallavibhavani1@gmail.com>
2026-07-04 20:55:21 +0530

    created first REST API

---
+++ backend/src/main/java/com/shopyra/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.shopyra.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/api/test")
+	public String testApi() {
+		return "Welcome to Shopyra Backend!";
+	}
+
+}
